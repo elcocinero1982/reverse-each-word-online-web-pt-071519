7e9a9337ad0efe9fb398f731b56ca3cd04db3c58
jose luis <cortezluisjose@gmail.com>
2019-07-24 23:19:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ reverse_each_word = ["Hello there, and how are you?"]
 
 def reverse(words)
   reverse_each_word.each do |reverse_each_word|
-  puts 
+  puts "olleH ,ereht dna woh era ?uoy"
+
   
 end
